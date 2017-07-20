d0038f15e682c2f792149c49b1626bcf52245933
binod44 <binod.kc@concordusa.com>
2017-07-20 09:32:25 -0500

    Added Failure Test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -18,6 +19,14 @@ public class HelloMavenTest extends FunctionalTestCase {
 		runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
+	 @Test
+	 public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+		 System.out.println("Dynamic Port in Test 2 : " + dynamicPort.getNumber() + "\n\n");
+		 MuleEvent event = runFlow("retrieveFlights");
+		 String contentType = event.getMessage().getOutboundProperty("Content-Type");
+		 assertEquals("application/json", contentType);
+	 }
+
     @Override
     protected String [] getConfigFiles() {
         return new String[]{"maven-project.xml","global.xml"};
